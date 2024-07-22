f5b87749788b5509768916eaa6a379f60983b34b
Aliyawar12 <aliyawar734@gmail.com>
2024-07-22 19:28:06 +0500

    Basic Theme Setting

---
+++ resources/views/pages/menu.blade.php
@@ -0,0 +1,193 @@
+@extends('layout.app')
+@section('title')
+ 
+@section('content')
+<div class="main-content" data-aos="fade-down" data-aos-duration="1500">
+    <div class="title">
+        <h2>Menu</h2>
+    </div>
+</div>
+<div class="promo text-center my-5 py-5">
+    <h5 class="hot">Hot Promo</h5>
+    <h2 class="special">Special offer on Sunday</h2>
+    <div class="container mt-5 pt-5">
+        <div class="row" data-aos="fade-up" data-aos-duration="1500">
+            <div class="col-md-3">
+                <a href="order.html"><img src="assets/theme/images/wafel.jpg" width="100%" alt=""></a>
+                <h2>Waffles</h2>
+            </div>
+            <div class="col-md-3">
+                <img src="assets/theme/images/shake.jpg" width="100%" alt="">
+                <h2>Shakes</h2>
+            </div>
+            <div class="col-md-3">
+                <img src="assets/theme/images/role.jpg" width="100%" alt="">
+                <h2>Profiteroles</h2>
+            </div>
+            <div class="col-md-3">
+                <img src="assets/theme/images/pancake.webp" width="100%" alt="">
+                <h2>Pancake</h2>
+            </div>
+        </div>
+        <div class="row" data-aos="fade-up" data-aos-duration="1500">
+            <div class="col-md-3">
+                <img src="assets/theme/images/dognut.webp" width="100%" alt="">
+                <h2>Mini Doughnuts</h2>
+            </div>
+            <div class="col-md-3">
+                <img src="assets/theme/images/lana-meal.webp" width="100%" alt="">
+                <h2>Lana Meal Deal</h2>
+            </div>
+            <div class="col-md-3">
+                <img src="assets/theme/images/ice-cream.webp" width="100%" alt="">
+                <h2>Ice Creams</h2>
+            </div>
+            <div class="col-md-3">
+                <img src="assets/theme/images/drinks.webp" width="100%" alt="">
+                <h2>Drinks</h2>
+            </div>
+        </div>
+        <div class="row" data-aos="fade-up" data-aos-duration="1500">
+            <div class="col-md-3">
+                <img src="assets/theme/images/croffles.webp" width="100%" alt="">
+                <h2>Croffles</h2>
+            </div>
+            <div class="col-md-3">
+                <img src="assets/theme/images/cookie-douugh.webp" width="100%" alt="">
+                <h2>Cookie Dough</h2>
+            </div>
+            <div class="col-md-3">
+                <img src="assets/theme/images/cinnamon-rol.png" width="100%" alt="">
+                <h2>Cinnamon Rolls</h2>
+            </div>
+            <div class="col-md-3">
+                <img src="assets/theme/images/chicken.png" width="100%" alt="">
+                <h2>Chicken</h2>
+            </div>
+        </div>
+        <div class="row" data-aos="fade-up" data-aos-duration="1500">
+            <div class="col-md-3">
+                <img src="assets/theme/images/cheesecake.webp" width="100%" alt="">
+                <h2>Cheesecakes</h2>
+            </div>
+            <div class="col-md-3">
+                <img src="assets/theme/images/burgers.png" width="100%" alt="">
+                <h2>Burgers</h2>
+            </div>
+            <div class="col-md-3">
+                <img src="assets/theme/images/brownies.jpg" width="100%" alt="">
+                <h2>Brownies</h2>
+            </div>
+            <div class="col-md-3">
+                <img src="assets/theme/images/blondies.webp" width="100%" alt="">
+                <h2>Blondies</h2>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="menu-items my-5 pt-5" data-aos="fade-down-right" data-aos-duration="1500">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <div class="dish-menu">
+                 <h2>Appertizer</h2>
+                 <p><strong>Salad <span>....................................................</span> $50</strong></p>
+                 <p class="dim-para">Sed ut perspiciatis unde omnis iste natus ero</p>
+                 <p><strong>Salad <span>....................................................</span> $50</strong></p>
+                 <p class="dim-para">Sed ut perspiciatis unde omnis iste natus ero</p>
+                 <p><strong>Salad <span>....................................................</span> $50</strong></p>
+                 <p class="dim-para">Sed ut perspiciatis unde omnis iste natus ero</p>
+                 <p><strong>Salad <span>....................................................</span> $50</strong></p>
+                 <p class="dim-para">Sed ut perspiciatis unde omnis iste natus ero</p>
+                 <p><strong>Salad <span>....................................................</span> $50</strong></p>
+                 <p class="dim-para">Sed ut perspiciatis unde omnis iste natus ero</p>
+                </div>
+               
+            </div>
+            <div class="col-md-4">
+                <div class="dish-img d-grid">
+                    <img src="assets/theme/images/dish-menu1.webp" class="dish-one" width="350px" alt="">
+                    <img src="assets/theme/images/dish-menu2.webp" class="mt-4 dish-two" width="350px" alt="">
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="right-side-dish d-grid">
+                    <img src="assets/theme/images/dish-menu3.webp" class="dish-three" width="100%" alt="">
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="menu-more my-5 pt-5" data-aos="fade-up-right" data-aos-duration="1500">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <div class="dish-more-img d-grid">
+                    <img src="assets/theme/images/more-dish1.webp" class="dish-one" width="350px" alt="">
+                    <img src=" assets/theme/images/menu-more3.webp" class="mt-4 dish-two" width="350px" alt="">
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="right-more-dish d-grid">
+                    <img src="assets/theme/images/menu-more2.webp" class="dish-three" width="500px" alt="">
+                    
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="dish-more">
+                 <h2>Main Course</h2>
+                 <p><strong>Sirloin Steak <span>....................................................</span> $50</strong></p>
+                 <p class="dim-para">Sed ut perspiciatis unde omnis iste natus ero</p>
+                 <p><strong>Korean soup <span>....................................................</span> $50</strong></p>
+                 <p class="dim-para">Sed ut perspiciatis unde omnis iste natus ero</p>
+                 <p><strong>Salmon pasta <span>....................................................</span> $50</strong></p>
+                 <p class="dim-para">Sed ut perspiciatis unde omnis iste natus ero</p>
+                 <p><strong>chicken curry <span>....................................................</span> $50</strong></p>
+                 <p class="dim-para">Sed ut perspiciatis unde omnis iste natus ero</p>
+                 <p><strong>Dimsum <span>....................................................</span> $50</strong></p>
+                 <p class="dim-para">Sed ut perspiciatis unde omnis iste natus ero</p>
+                </div>
+               
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="menu-items my-5 pt-5" data-aos="fade-down-right" data-aos-duration="1500">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <div class="dish-menu">
+                 <h2>Dessert</h2>
+                 <p><strong>Pancake<span>....................................................</span> $50</strong></p>
+                 <p class="dim-para">Sed ut perspiciatis unde omnis iste natus ero</p>
+                 <p><strong>Ice Cream <span>....................................................</span> $50</strong></p>
+                 <p class="dim-para">Sed ut perspiciatis unde omnis iste natus ero</p>
+                 <p><strong>Sambosa <span>....................................................</span> $50</strong></p>
+                 <p class="dim-para">Sed ut perspiciatis unde omnis iste natus ero</p>
+                 <p><strong>American pie <span>....................................................</span> $50</strong></p>
+                 <p class="dim-para">Sed ut perspiciatis unde omnis iste natus ero</p>
+                 <p><strong>Banoffie <span>....................................................</span> $50</strong></p>
+                 <p class="dim-para">Sed ut perspiciatis unde omnis iste natus ero</p>
+                </div>
+               
+            </div>
+            <div class="col-md-4">
+                <div class="dish-img d-grid">
+                    <img src="assets/theme/images/dish-menu1.webp" class="dish-one" width="350px" alt="">
+                    <img src="assets/theme/images/dish-menu2.webp" class="mt-4 dish-two" width="350px" alt="">
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="right-side-dish d-grid">
+                    <img src="assets/theme/images/dish-menu3.webp" class="dish-three" width="100%" alt="">
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ContactController;
+use App\Http\Controllers\MenuController;
+use App\Http\Controllers\FAQController;
+
+
+Route::get('/', [DashboardController::class, 'index'])->name('dashboad');
+Route::get('/about', [AboutController::class, 'index'])->name('about');
+Route::get('/contact', [ContactController::class, 'index'])->name('contact');
+Route::get('/menu', [MenuController::class, 'index'])->name('menu');
+Route::get('/faq', [FAQController::class, 'index'])->name('faq');
