8a1018747ec3c2b430686051fe862125bcab7c82
Usman <it.dev45@dawaateislami.net>
2024-07-24 22:07:34 +0500

    fetch menu using api

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,12 +3,26 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class DashboardController extends Controller
 
 {
     public function index()
     {
-        return view ('dashboard');
+        $serverUrl = env('SERVER_URL');
+        $apiToken = env('API_TOKEN');
+        
+        $response = Http::withHeaders([
+            'Authorization' => $apiToken,
+        ])->get($serverUrl . 'api/menu');
+
+        $data['menu'] = $response['data'];
+
+        $collection = collect($response['data']);
+        $data['group'] = $collection->groupBy('category_id');
+        $data['menus'] = $collection->pluck('category.name',)->unique()->values()->all();
+        
+        return view ('dashboard', $data);
     }
 }
