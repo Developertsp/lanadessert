bfd1a6a612d9f2114e359253e6706de3b47d4111
Usman <it.dev45@dawaateislami.net>
2024-07-30 22:30:05 +0500

    add to cart work (not complete)

--- resources/views/include/script.blade.php
+++ resources/views/include/script.blade.php
@@ -1,4 +1,5 @@
 {{-- <script src="{{ asset('assets/theme/js/main.js') }}"></script>  --}}
+<script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
 <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\AboutController;
 use App\Http\Controllers\ApiController;
+use App\Http\Controllers\CartController;
 use App\Http\Controllers\ContactController;
 use App\Http\Controllers\MenuController;
 use App\Http\Controllers\FAQController;
@@ -19,4 +20,7 @@ Route::get('/faq', [FAQController::class, 'index'])->name('faq');
 
 Route::get('categories', [ApiController::class, 'categories'])->name('categories.all');
 Route::get('menu', [ApiController::class, 'products'])->name('menu');
+
+Route::post('cart/add', [CartController::class, 'add'])->name('cart.add');
+Route::get('cart', [CartController::class, 'view'])->name('cart.view');
 
